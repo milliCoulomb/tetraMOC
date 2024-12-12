9fe439ee066e7c2f5390e4406b9e01bf0cc2779e
Universal-AC <mathis.caprais@gmail.com>
2024-12-12 15:58:25 +0100

    Add methods to MeshHandler class for loading nodes, cells, and face connectivity from files

---
+++ src/MeshHandler.cpp
@@ -0,0 +1,83 @@
+// src/mesh_handler.cpp
+#include "MeshHandler.hpp"
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+
+bool MeshHandler::loadNodes(const std::string& filename) {
+    std::ifstream infile(filename);
+    if (!infile.is_open()) {
+        std::cerr << "Error: Unable to open nodes file " << filename << std::endl;
+        return false;
+    }
+    
+    int num_nodes;
+    infile >> num_nodes;
+    nodes.resize(num_nodes);
+    
+    for(int i = 0; i < num_nodes; ++i) {
+        infile >> nodes[i].x >> nodes[i].y >> nodes[i].z;
+        if(infile.fail()) {
+            std::cerr << "Error: Invalid format in nodes file at line " << i+2 << std::endl;
+            return false;
+        }
+    }
+    
+    infile.close();
+    return true;
+}
+
+bool MeshHandler::loadCells(const std::string& filename) {
+    std::ifstream infile(filename);
+    if (!infile.is_open()) {
+        std::cerr << "Error: Unable to open cells file " << filename << std::endl;
+        return false;
+    }
+    
+    int num_cells;
+    infile >> num_cells;
+    cells.resize(num_cells);
+    
+    for(int i = 0; i < num_cells; ++i) {
+        infile >> cells[i].node_ids[0] >> cells[i].node_ids[1] >> cells[i].node_ids[2] >> cells[i].node_ids[3];
+        cells[i].cell_id = i;
+        if(infile.fail()) {
+            std::cerr << "Error: Invalid format in cells file at line " << i+2 << std::endl;
+            return false;
+        }
+    }
+    
+    infile.close();
+    return true;
+}
+
+bool MeshHandler::loadFaceConnectivity(const std::string& filename) {
+    std::ifstream infile(filename);
+    if(!infile.is_open()) {
+        std::cerr << "Error: Unable to open face connectivity file " << filename << std::endl;
+        return false;
+    }
+    
+    std::string line;
+    while(std::getline(infile, line)) {
+        std::istringstream iss(line);
+        int n0, n1, n2;
+        std::vector<int> cells;
+        if(!(iss >> n0 >> n1 >> n2)) {
+            std::cerr << "Error: Invalid format in faces file at line: " << line << std::endl;
+            continue;
+        }
+        int cell_id;
+        while(iss >> cell_id) {
+            cells.push_back(cell_id);
+        }
+        // Ensure the face is sorted
+        std::array<int, 3> sorted_nodes = {n0, n1, n2};
+        std::sort(sorted_nodes.begin(), sorted_nodes.end());
+        faces.emplace_back(std::make_tuple(sorted_nodes[0], sorted_nodes[1], sorted_nodes[2]), cells);
+    }
+    
+    infile.close();
+    return true;
+}
