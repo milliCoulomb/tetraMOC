85b9b1ceba3a8582434ae2830f7206d39ab8b7a4
Universal-AC <mathis.caprais@gmail.com>
2024-12-16 13:51:24 +0100

    Remove Velocity structure and related member from MeshHandler class

--- include/MeshHandler.hpp
+++ include/MeshHandler.hpp
@@ -19,11 +19,6 @@ struct TetraCell {
     int cell_id;
 };
 
-// Structure to store velocity vectors
-struct Velocity {
-    double vx, vy, vz;
-};
-
 // MeshHandler class to manage mesh data
 class MeshHandler {
 public:
@@ -33,19 +28,16 @@ public:
     bool loadNodes(const std::string& filename);
     bool loadCells(const std::string& filename);
     bool loadFaceConnectivity(const std::string& filename);
-    bool loadVelocityField(const std::string& filename);
     
     // Getters for accessing mesh data
     const std::vector<Node>& getNodes() const { return nodes; }
     const std::vector<TetraCell>& getCells() const { return cells; }
     const std::vector<std::tuple<int, int, int, std::vector<int>>>& getFaces() const { return faces; }
-    const std::vector<Velocity>& getVelocities() const { return velocities; }
 
 private:
     std::vector<Node> nodes;
     std::vector<TetraCell> cells;
     std::vector<std::tuple<int, int, int, std::vector<int>>> faces; // (n0, n1, n2, [cell_ids])
-    std::vector<Velocity> velocities;
 };
 
 #endif // MESH_HANDLER_H
