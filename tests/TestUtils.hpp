6c27d3f06844ca60936c866db1217ed36ec699bf
Universal-AC <mathis.caprais@gmail.com>
2024-12-16 16:30:11 +0100

    Add utility functions for temporary file creation and unique filename generation

--- tests/TestUtils.hpp
+++ tests/TestUtils.hpp
@@ -3,7 +3,13 @@
 #define TEST_UTILS_HPP
 
 #include <string>
+#include <fstream>
+#include <cstdio>
+#include <cstdlib>
 
+namespace SNSolver {
+
+// Utility function to create a temporary file with given content
 inline bool createTempFile(const std::string& filename, const std::string& content) {
     std::ofstream outfile(filename);
     if (!outfile.is_open()) return false;
@@ -11,5 +17,16 @@ inline bool createTempFile(const std::string& filename, const std::string& conte
     outfile.close();
     return true;
 }
+
+// Utility function to generate a unique temporary filename
+inline std::string generateUniqueFilename(const std::string& prefix = "temp_", const std::string& suffix = ".txt") {
+    // Generate a unique filename using rand and time
+    std::string filename = prefix;
+    filename += std::to_string(std::rand());
+    filename += suffix;
+    return filename;
+}
+
+} // namespace SNSolver
 
 #endif // TEST_UTILS_HPP
