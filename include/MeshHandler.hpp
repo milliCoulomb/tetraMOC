88dab70478bc17a52d7bd3b4afed2f1b5ac75a8c
Universal-AC <mathis.caprais@gmail.com>
2024-12-16 11:08:38 +0100

    Add velocity field support to MeshHandler class

--- include/MeshHandler.hpp
+++ include/MeshHandler.hpp
@@ -1,4 +1,5 @@
-// include/mesh_handler.h
+// include/MeshHandler.hpp
+
 #ifndef MESH_HANDLER_H
 #define MESH_HANDLER_H
 
@@ -7,30 +8,44 @@
 #include <array>
 #include <tuple>
 
+// Structure to store node coordinates
 struct Node {
     double x, y, z;
 };
 
+// Structure to store tetrahedral cell connectivity
 struct TetraCell {
     std::array<int, 4> node_ids;
     int cell_id;
 };
 
+// Structure to store velocity vectors
+struct Velocity {
+    double vx, vy, vz;
+};
+
+// MeshHandler class to manage mesh data
 class MeshHandler {
 public:
     MeshHandler() = default;
+
+    // Methods to load mesh data
     bool loadNodes(const std::string& filename);
     bool loadCells(const std::string& filename);
     bool loadFaceConnectivity(const std::string& filename);
-
+    bool loadVelocityField(const std::string& filename);
+    
+    // Getters for accessing mesh data
     const std::vector<Node>& getNodes() const { return nodes; }
     const std::vector<TetraCell>& getCells() const { return cells; }
     const std::vector<std::tuple<int, int, int, std::vector<int>>>& getFaces() const { return faces; }
+    const std::vector<Velocity>& getVelocities() const { return velocities; }
 
 private:
     std::vector<Node> nodes;
     std::vector<TetraCell> cells;
     std::vector<std::tuple<int, int, int, std::vector<int>>> faces; // (n0, n1, n2, [cell_ids])
+    std::vector<Velocity> velocities;
 };
 
 #endif // MESH_HANDLER_H
