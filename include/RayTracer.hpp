6ce6697f1a3ffc624833f44544390cd0dac0a660
Universal-AC <mathis.caprais@gmail.com>
2024-12-17 15:32:11 +0100

    Refactor RayTracer: Update traceRay method to use CellTrace instead of DirectionData for consistency

--- include/RayTracer.hpp
+++ include/RayTracer.hpp
@@ -6,6 +6,7 @@
 #include "Field.hpp"
 #include "Tetrahedron.hpp"
 #include "Vector3D.hpp" // Ensure this defines SNSolver::Vector3D
+#include <TrackingData.hpp>
 #include <vector>
 #include <array>
 #include <unordered_map>
@@ -15,7 +16,7 @@
 class RayTracer {
 public:
     RayTracer(const MeshHandler& mesh, const Field& field);
-    std::vector<DirectionData> traceRay(int start_cell_id, const std::array<double, 3>& start_point, int max_iter = 100) const;
+    std::vector<CellTrace> traceRay(int start_cell_id, const std::array<double, 3>& start_point, int max_iter = 100) const;
     
 private:
     const MeshHandler& mesh;
