eee002465385667426410af7757e7a29923f2f86
Universal-AC <mathis.caprais@gmail.com>
2024-12-17 15:32:39 +0100

    Refactor GeometryUtils: Remove unnecessary namespace wrapping for clarity

--- include/GeometryUtils.hpp
+++ include/GeometryUtils.hpp
@@ -5,14 +5,10 @@
 #include <array>
 #include "MeshHandler.hpp" // Include MeshHandler to access the Node struct
 
-namespace SNSolver {
-
 // Function to compute the normal of a triangle face
 std::array<double, 3> computeFaceNormal(const std::array<Node, 3>& triangle);
 
 // Function to sample a point uniformly within a triangle
 std::array<double, 3> samplePointOnTriangle(const std::array<Node, 3>& triangle);
 
-} // namespace SNSolver
-
-#endif // GEOMETRY_UTILS_HPP
+#endif // GEOMETRY_UTILS_HPP 
