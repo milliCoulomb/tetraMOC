8e5172462b0308ada334ebeda47c73f8b94a113d
milliCoulomb <mathis.caprais@gmail.com>
2024-12-18 14:29:14 +0100

    Refactor GeometryUtils: use Vector3D class to simplify the normal calculation

--- src/GeometryUtils.cpp
+++ src/GeometryUtils.cpp
@@ -1,32 +1,37 @@
 // src/GeometryUtils.cpp
 #include "GeometryUtils.hpp"
+#include "Vector3D.hpp"
 #include <cmath>
 #include <random>
 #include <thread>
 
-Vector3D computeFaceNormal(const std::array<Node, 3>& triangle) {
+Vector3D computeFaceNormal(const std::array<Vector3D, 3>& triangle) {
     // Vectors from the first node
-    double ux = triangle[1].x - triangle[0].x;
-    double uy = triangle[1].y - triangle[0].y;
-    double uz = triangle[1].z - triangle[0].z;
+    // double ux = triangle[1].x - triangle[0].x;
+    // double uy = triangle[1].y - triangle[0].y;
+    // double uz = triangle[1].z - triangle[0].z;
 
-    double vx = triangle[2].x - triangle[0].x;
-    double vy = triangle[2].y - triangle[0].y;
-    double vz = triangle[2].z - triangle[0].z;
+    Vector3D u = triangle[1] - triangle[0];
+    Vector3D v = triangle[2] - triangle[0];
+    Vector3D n = u.cross(v);
 
-    // Cross product to get normal
-    double nx = uy * vz - uz * vy;
-    double ny = uz * vx - ux * vz;
-    double nz = ux * vy - uy * vx;
+    // double vx = triangle[2].x - triangle[0].x;
+    // double vy = triangle[2].y - triangle[0].y;
+    // double vz = triangle[2].z - triangle[0].z;
 
-    // Normalize the normal vector
-    double norm = std::sqrt(nx * nx + ny * ny + nz * nz);
+    // // Cross product to get normal
+    // double nx = uy * vz - uz * vy;
+    // double ny = uz * vx - ux * vz;
+    // double nz = ux * vy - uy * vx;
+
+    // // Normalize the normal vector
+    double norm = n.norm();
     if(norm == 0) return {0.0, 0.0, 0.0}; // Degenerate triangle
 
-    return Vector3D(nx / norm, ny / norm, nz / norm);
+    return n.normalized();
 }
 
-Vector3D samplePointOnTriangle(const std::array<Node, 3>& triangle) {
+Vector3D samplePointOnTriangle(const std::array<Vector3D, 3>& triangle) {
     // Thread-local random number generator for thread safety
     thread_local std::mt19937 generator(std::random_device{}());
     std::uniform_real_distribution<double> distribution(0.0, 1.0);
