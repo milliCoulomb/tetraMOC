1d055246181ba76a878d7d09fec01ecd51d6971b
milliCoulomb <mathis.caprais@gmail.com>
2024-12-18 14:33:43 +0100

    remove MeshHandler dependance from GeometryUtils

--- include/GeometryUtils.hpp
+++ include/GeometryUtils.hpp
@@ -3,7 +3,6 @@
 #define GEOMETRY_UTILS_HPP
 
 #include <array>
-#include "MeshHandler.hpp" // Include MeshHandler to access the Node struct
 #include "Vector3D.hpp"    // Include Vector3D to access the Vector3D class
 
 // Function to compute the normal of a triangle face
