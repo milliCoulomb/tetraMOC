32a58c1040ded45ee65889737898ec6371e75377
milliCoulomb <mathis.caprais@gmail.com>
2024-12-20 19:07:05 +0100

    Update computeFaceNormal function to include cell center parameter

--- include/GeometryUtils.hpp
+++ include/GeometryUtils.hpp
@@ -6,7 +6,7 @@
 #include "Vector3D.hpp"    // Include Vector3D to access the Vector3D class
 
 // Function to compute the normal of a triangle face
-Vector3D computeFaceNormal(const std::array<Vector3D, 3>& triangle);
+Vector3D computeFaceNormal(const std::array<Vector3D, 3>& triangle, const Vector3D& cell_center);
 
 // Function to sample a point uniformly within a triangle
 Vector3D samplePointOnTriangle(const std::array<Vector3D, 3>& triangle);
