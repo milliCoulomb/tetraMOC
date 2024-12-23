572ee9036eebdff291fef7ea800707e75b83e562
milliCoulomb <mathis.caprais@gmail.com>
2024-12-23 09:25:13 +0100

    Add conditional logging support in Logger class based on DISABLE_LOGGING flag

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -1,6 +1,8 @@
 // Utilities/Logger.cpp
 #include "Logger.hpp"
 
+#ifndef DISABLE_LOGGING
+
 void Logger::log(const std::string& message, LogLevel level) {
     switch(level) {
         case LogLevel::INFO:
@@ -26,3 +28,23 @@ void Logger::warning(const std::string& message) {
 void Logger::error(const std::string& message) {
     log(message, LogLevel::ERROR);
 }
+
+#else // DISABLE_LOGGING is defined
+
+void Logger::log(const std::string&, LogLevel) {
+    // Logging is disabled; do nothing
+}
+
+void Logger::info(const std::string&) {
+    // Logging is disabled; do nothing
+}
+
+void Logger::warning(const std::string&) {
+    // Logging is disabled; do nothing
+}
+
+void Logger::error(const std::string&) {
+    // Logging is disabled; do nothing
+}
+
+#endif // DISABLE_LOGGING
