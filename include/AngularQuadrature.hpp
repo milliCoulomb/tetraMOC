9c6d42cdf2b26b90e4d2f8573aae2881e52247dd
milliCoulomb <mathis.caprais@gmail.com>
2024-12-29 18:14:39 +0100

    Enhance documentation for Direction struct and AngularQuadrature class constructors

--- include/AngularQuadrature.hpp
+++ include/AngularQuadrature.hpp
@@ -6,13 +6,13 @@
 #include "Quadrature.hpp"
 #include <utility>
 
-/*
-@struct Direction
-@brief Structure to store the direction of the quadrature and its weight
-
-@var mu: cosine of the polar angle
-@var phi: azimuthal angle
-@var weight: weight of the direction
+/**
+ * @struct Direction
+ * @brief Structure to store the direction of the quadrature and its weight
+ * 
+ * @param mu: cosine of the polar angle
+ * @param phi: azimuthal angle
+ * @param weight: weight of the direction
 */
 
 struct Direction {
@@ -23,15 +23,45 @@ struct Direction {
 
 class AngularQuadrature {
 public:
+    /**
+    @brief Constructor for the AngularQuadrature class
+
+    @param theta_order: number of polar angles
+    @param phi_order: number of azimuthal angles
+    */
     AngularQuadrature(int theta_order, int phi_order);
     // surcharge constructor to be able to build an angular quadrature with a std vector of directions
+
+    /**
+     * @brief Constructor for the AngularQuadrature class
+     * 
+     * @param directions: vector of directions
+     */
     AngularQuadrature(std::vector<Direction> directions);
+
     
     ~AngularQuadrature() = default;
 
+    /**
+     * @brief Get the directions of the quadrature
+     * 
+     * @return std::vector<Direction> vector of directions
+     */
     const std::vector<Direction>& getDirections() const;
 
+    /**
+     * @brief add a direction to the quadrature
+     * 
+     * @param direction: direction to add
+     */
+
     void addDirection(const Direction& direction);
+
+    /**
+     * @brief Calculate the total weight of the quadrature
+     * 
+     * @return double total weight
+     */
 
     double getTotalWeight() const;
 
